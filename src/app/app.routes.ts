5cc0a53f95ea404575e0db7027e0ab133433330a
meyatech <surafelmegist2015@gmail.com>
2026-07-30 07:48:05 -0700

    add mdule 8 session 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,62 @@
+// import { Routes } from '@angular/router';
+// import { StudentDashboardComponent } from './features/student-dashboard/student-dashboard.component';
+
+// export const routes: Routes = [
+
+//   {
+//     path: 'dashboard',
+//     component: StudentDashboardComponent
+//   },
+
+//   {
+//     path: 'courses/:id',
+//     loadComponent: () =>
+//       import('./features/course-detail/course-detail')
+//         .then(m => m.CourseDetail)
+//   },
+
+//   {
+//     path: '',
+//     redirectTo: 'dashboard',
+//     pathMatch: 'full'
+//   }
+
+
+
+
+
 import { Routes } from '@angular/router';
 import { StudentDashboardComponent } from './features/student-dashboard/student-dashboard.component';
 
+
 export const routes: Routes = [
 
-  {
-    path: 'dashboard',
-    component: StudentDashboardComponent
-  },
+{
+ path:'dashboard',
+ component:StudentDashboardComponent
+},
 
-  {
-    path: 'courses/:id',
-    loadComponent: () =>
-      import('./features/course-detail/course-detail')
-        .then(m => m.CourseDetail)
-  },
 
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
+{
+ path:'enroll',
+ loadComponent:()=> 
+ import('./features/enrollment-form/enrollment-form')
+ .then(m=>m.EnrollmentFormComponent)
+},
+
+
+{
+  path: 'courses/:id',
+  loadComponent: () =>
+    import('./features/course-detail/course-detail')
+      .then(m => m.CourseDetailComponent)
+},
+
+{
+ path:'',
+ redirectTo:'dashboard',
+ pathMatch:'full'
+}
+
 
 ];
--- src/app/features/course-detail/course-detail.ts
+++ src/app/features/course-detail/course-detail.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CourseService } from '../../services/course.service';
-import { Course } from '../../models/course.model';
+import { CourseDetail } from '../../models/course.model';
 
 @Component({
   selector: 'app-course-detail',
@@ -9,26 +9,29 @@ import { Course } from '../../models/course.model';
   templateUrl: './course-detail.html',
   styleUrl: './course-detail.scss'
 })
-export class CourseDetail {
+export class CourseDetailComponent {
 
   private route = inject(ActivatedRoute);
   private courseService = inject(CourseService);
 
-  course?: Course;
-
+  course?: CourseDetail;
 
   constructor() {
+    const id = this.route.snapshot.paramMap.get('id');
 
-    const id = Number(
-      this.route.snapshot.paramMap.get('id')
-    );
+    if (id) {
+      console.log('Route ID:', id);
 
-    console.log('Route ID:', id);
-
-    this.course = this.courseService.getCourseById(id);
-
-    console.log('Course found:', this.course);
-
+      this.courseService.getById(id).subscribe({
+        next: (course) => {
+          this.course = course;
+          console.log('Course found:', course);
+        },
+        error: (err) => {
+          console.error('Failed to load course:', err);
+        }
+      });
+    }
   }
 
 }
